92c916b62603232b8cb1ace298b66fba22ad0e8a
Dmitry Kamarov <dk@anti-virus.by>
2013-06-27 17:49:20 +0300

    Исправлено определение статуса устройства в процедуре GetDevicesPageForGroup.

--- src/db_script/GroupsDevices.sql
+++ src/db_script/GroupsDevices.sql
@@ -29,13 +29,14 @@ SELECT [ID] FROM TreeGroups
 DECLARE @DevicesPage TABLE(
 			[RecID] int IDENTITY(1, 1) NOT NULL,									
 			[D_ID] smallint,
-			[DP_LatestInsert] smalldatetime
+			[DP_LatestInsert] smalldatetime,
+			[CountStates] smallint
 		)
 	
-INSERT INTO @DevicesPage([D_ID], [DP_LatestInsert])
+INSERT INTO @DevicesPage([D_ID], [DP_LatestInsert], [CountStates])
 
 SELECT
-	d.[ID], MAX(dp.[LatestInsert])
+	d.[ID], MAX(dp.[LatestInsert]), COUNT(DISTINCT(dp.[DevicePolicyStateID]))
 FROM @GroupPage AS gp
 INNER JOIN Groups AS g ON g.[GroupID] = gp.[ID]
 INNER JOIN DevicesPolicies AS dp ON dp.[ComputerID] = g.[ComputerID]
@@ -43,7 +44,10 @@ INNER JOIN Devices AS d ON d.[ID] = dp.[DeviceID]
 GROUP BY d.[ID]
 
 SELECT 
-	dp.[ID], c.[ID], c.[ComputerName], d.[ID], dps.[StateName],
+	dp.[ID], c.[ID], c.[ComputerName], d.[ID], StateName=CASE tmp.[CountStates]
+	WHEN 1 THEN dps.[StateName]
+	ELSE 'Undefined'
+    END,
 	d.[SerialNo], dt.[TypeName], d.[Comment], dp.[LatestInsert]
 FROM @DevicesPage AS tmp
 INNER JOIN Devices AS d ON d.[ID] = tmp.[D_ID]
