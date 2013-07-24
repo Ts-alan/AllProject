1fe8b323ff2b2a2723fee17e922c99ab3fbad746
Dmitry Kamarov <dk@anti-virus.by>
2013-07-24 18:41:43 +0300

    Добавлена хранимая процедура для вытягивания списка компов по устройству

--- src/db_script/GroupsDevices.sql
+++ src/db_script/GroupsDevices.sql
@@ -587,3 +587,26 @@ AS
 
 	EXEC sp_executesql @Query
 GO
+
+
+-- Get ComputerListByDeviceID
+IF EXISTS (SELECT [ID] FROM dbo.sysobjects WHERE [ID] = OBJECT_ID(N'[dbo].[GetComputerListByDeviceID]')
+					   AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
+DROP PROCEDURE [dbo].[GetComputerListByDeviceID]
+GO
+
+CREATE PROCEDURE [dbo].[GetComputerListByDeviceID]
+	@DeviceID smallint		
+WITH ENCRYPTION
+AS
+
+	SELECT 
+		dp.[ID], c.[ID], c.[ComputerName], dps.[StateName], dp.[LatestInsert], g.[GroupID]
+	FROM DevicesPolicies AS dp
+	INNER JOIN DevicePolicyStates AS dps ON dps.[ID] = dp.[DevicePolicyStateID]
+	INNER JOIN Computers AS c ON c.[ID] = dp.[ComputerID]
+	LEFT JOIN Groups AS g ON g.[ComputerID] = dp.[ComputerID]	
+	WHERE dp.[DeviceID] = @DeviceID
+	ORDER BY c.[ComputerName] ASC
+
+GO
