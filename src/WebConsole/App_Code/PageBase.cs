7fac1d7199740d451f4a8c9545826b87aafa8cfe
Dmitry Kamarov <dk@anti-virus.by>
2013-08-10 18:12:26 +0300

    PageBase.cs: - Refactoring

--- src/WebConsole/App_Code/PageBase.cs
+++ src/WebConsole/App_Code/PageBase.cs
@@ -9,6 +9,7 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using System.Globalization;
 using System.Threading;
+using System.Text;
 
 //using VirusBlokAda.Vba32CC.Policies;
 
@@ -19,49 +20,70 @@ public abstract class PageBase : PackViewState.Page
 {
 	public PageBase()
 	{
-		//
-		// TODO: Add constructor logic here
-		//
 	}
 
-    protected void RegisterScript(string src)
+    #region Registering
+
+    protected void RegisterScript(String src)
     {
         HtmlGenericControl include = new HtmlGenericControl("script");
         include.Attributes.Add("type", "text/javascript");
         include.Attributes.Add("src", src);
         this.Page.Header.Controls.Add(include);
     }
-    protected void RegisterBlockScript(string scriptText)
+
+    protected void RegisterBlockScript(String scriptText)
     {
         HtmlGenericControl include = new HtmlGenericControl("script");
         include.Attributes.Add("type", "text/javascript");
-        include.Attributes.Add("runat", "server");
         include.InnerHtml = scriptText;        
         this.Page.Header.Controls.Add(include);
     }
-    protected void RegisterLink(string src)
+
+    protected void RegisterLink(String src)
     {
         HtmlGenericControl include = new HtmlGenericControl("link");
         include.Attributes.Add("type", "text/css");
         include.Attributes.Add("src", src);
         this.Page.Header.Controls.Add(include);
     }
-    
+
+    /// <summary>
+    /// Register resources for using in JavaScript
+    /// </summary>
+    /// <param name="resources"></param>
+    protected void ResourceRegister(String[] resources)
+    {
+        StringBuilder script = new StringBuilder("var Resource={");
+
+        for (Int32 index = 0; index < resources.Length; index++)
+        {
+            script.AppendFormat("{0}:'{1}'", resources[index], ResourceControl.GetStringForCurrentCulture(resources[index]));
+            if (index != resources.Length - 1)
+                script.Append(", ");
+        }        
+        script.Append("}");
+        RegisterBlockScript(script.ToString());
+    }
+
+    #endregion
+
     protected void Page_PreInit(object sender, EventArgs e)
     {
-        Page.MasterPageFile = (string) HttpContext.Current.Profile.GetPropertyValue("MasterPage");
-        Page.Theme = (string)HttpContext.Current.Profile.GetPropertyValue("Theme");
+        Page.MasterPageFile = (String) HttpContext.Current.Profile.GetPropertyValue("MasterPage");
+        Page.Theme = (String)HttpContext.Current.Profile.GetPropertyValue("Theme");
     }
+
     protected void Page_Init(object sender, EventArgs e)
     {
-         RegisterLink("~/App_Themes/" + (string)HttpContext.Current.Profile.GetPropertyValue("Theme") + @"/ui/jquery-ui-1.10.3.custom.min.css");
+        RegisterLink("~/App_Themes/" + (String)HttpContext.Current.Profile.GetPropertyValue("Theme") + @"/ui/jquery-ui-1.10.3.custom.min.css");
         RegisterScript(@"js/jQuery/jquery-1.10.2.min.js");
-        RegisterScript(@"js/jQuery/jquery-ui-1.10.3.custom.min.js"); 
+        RegisterScript(@"js/jQuery/jquery-ui-1.10.3.custom.min.js");
     }
    
     protected override void InitializeCulture()
     {
-        CultureInfo culture = new CultureInfo((string)HttpContext.Current.Profile.GetPropertyValue("Culture"));
+        CultureInfo culture = new CultureInfo((String)HttpContext.Current.Profile.GetPropertyValue("Culture"));
         Thread.CurrentThread.CurrentUICulture = culture;
         Thread.CurrentThread.CurrentCulture = culture;
         base.InitializeCulture();
