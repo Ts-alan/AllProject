a56da7667fccc4aa46427556dbaa5a8aaedcf913
Dmitry Kamarov <dk@anti-virus.by>
2013-10-18 16:43:22 +0300

    Добавлена хранимая процедура для страницы устройств (обновлено)

--- src/db_script/GroupsDevices.sql
+++ src/db_script/GroupsDevices.sql
@@ -710,10 +710,18 @@ DROP PROCEDURE [dbo].[GetSubgroupTypes]
 GO
 
 CREATE PROCEDURE [dbo].[GetSubgroupTypes]
-	@ParentID int
+	@ParentID int = NULL
 WITH ENCRYPTION
 AS
-	SELECT * FROM GroupTypes
-	WHERE [ParentID] = @ParentID
-	ORDER BY [GroupName]
+	IF @ParentID IS NULL
+	BEGIN
+		SELECT * FROM GroupTypes
+		WHERE [ParentID] IS NULL
+		ORDER BY [GroupName]
+	END
+	ELSE BEGIN
+		SELECT * FROM GroupTypes
+		WHERE [ParentID] = @ParentID
+		ORDER BY [GroupName]
+	END
 GO
