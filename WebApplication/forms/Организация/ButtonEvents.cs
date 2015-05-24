9b2200ce05f5985379abc4ee5c7f034d4a87a9c1
Denis <Ts-alan@mail.ru>
2015-05-24 12:17:14 +0200

    цццц

---
+++ WebApplication/forms/Организация/ButtonEvents.cs
@@ -0,0 +1,34 @@
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.UI;
+//using System.Web.UI.WebControls;
+
+//    public partial class Организация : Page
+//    {
+        ////========== EVENT FUNCTION ============
+        //public void Кнопка8_Click(object sender, EventArgs e)
+        //{
+        //    //TODO: Add your code here
+        //}
+
+        ////========== EVENT FUNCTION ============
+        //public void Кнопка6_Click(object sender, EventArgs e)
+        //{
+        //    Response.Redirect("/");
+        //}
+
+        ////========== EVENT FUNCTION ============
+        //public void Кнопка5_Click(object sender, EventArgs e)
+        //{
+        //    //TODO: Add your code here
+        //}
+
+        ////========== EVENT FUNCTION ============
+        //public void Кнопка4_Click(object sender, EventArgs e)
+        //{
+            
+        //}
+    //}
+
